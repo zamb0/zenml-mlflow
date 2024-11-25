08dae5c2f1f5f5bef8352881f1fc8eb2ac681d4a
zamb0 <zamboni.fabio23@gmail.com>
2024-11-26 00:29:36 +0100

    add dataset loader

---
+++ dataset.py
@@ -0,0 +1,10 @@
+# create the dataset for the model
+import os
+from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
+from PIL import Image
+
+ImageFolder(root='hymenoptera_data', transform=None, target_transform=None, loader=None)
+
+    
+
