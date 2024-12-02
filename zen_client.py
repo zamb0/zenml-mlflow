b0ffb21226854d758cafa450640baa70ac9154db
zamb0 <zamboni.fabio23@gmail.com>
2024-12-02 12:12:49 +0100

    remove comment

--- zen_client.py
+++ zen_client.py
@@ -6,4 +6,3 @@ mlflow.set_tracking_uri('http://127.0.0.1:5000')
 
 experiment_tracker = Client().active_stack.experiment_tracker
 print(Client().active_stack.experiment_tracker.get_tracking_uri())
-#mlflow.set_experiment(experiment_name=Config.experiment_name)
