c227367adcb0e8cb002f78d46537b5192d80c644
zamb0 <zamboni.fabio23@gmail.com>
2024-12-02 02:03:01 +0100

    remove mlflow uri

--- pipelines/training_pipe.py
+++ pipelines/training_pipe.py
@@ -4,13 +4,10 @@ from steps.ingest import ingest
 from steps.transform import transform
 from steps.train import train
 
-mlflow.set_tracking_uri('http://127.0.0.1:5000')
-
 @pipeline()
 def training_pipe(data_root:str="hymenoptera_data"):
       
     train_dataset, val_dataset = ingest(data_root)
     train_dataset, val_dataset = transform(train_dataset, val_dataset)
-    model = train(train_dataset, val_dataset)
-    
+    model, accuracy = train(train_dataset, val_dataset)
     #mlflow.pytorch.log_model(model, "model")
