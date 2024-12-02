a24b08ef370712613433d8a7b5e0b3a859a85e7d
zamb0 <zamboni.fabio23@gmail.com>
2024-12-02 01:59:03 +0100

    add params

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ class Config:
     step_size=7
     gamma=0.1
     batch_size=4
-    
+    num_epochs=25
+    min_accuracy=0.8
+    model_name='resnet18'
+    stack_name='mlflow_stack'
+    experiment_name='hymenoptera_experiment'
