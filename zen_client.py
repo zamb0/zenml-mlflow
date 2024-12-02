6637c8e1249640765e11967274715405af67ec9c
zamb0 <zamboni.fabio23@gmail.com>
2024-12-02 12:12:40 +0100

    fix zenml init

--- zen_client.py
+++ zen_client.py
@@ -4,6 +4,6 @@ from config import Config
 
 mlflow.set_tracking_uri('http://127.0.0.1:5000')
 
-experiment_tracker = Client().active_stack.experiment_tracker.name
-print(experiment_tracker)
-mlflow.set_experiment(experiment_name=experiment_tracker)
+experiment_tracker = Client().active_stack.experiment_tracker
+print(Client().active_stack.experiment_tracker.get_tracking_uri())
+#mlflow.set_experiment(experiment_name=Config.experiment_name)
