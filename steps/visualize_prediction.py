b063c8bc7587bb26b30f095fe5220ff303253836
zamb0 <zamboni.fabio23@gmail.com>
2024-12-02 12:15:20 +0100

    add logging to predicted labels

--- steps/visualize_prediction.py
+++ steps/visualize_prediction.py
@@ -8,8 +8,11 @@ def visualize_prediction(predictions: np.ndarray, true_label: np.ndarray) -> lis
     
     output = []
         
+    i=0
     for prediction, true in zip(predictions, true_label):
         output.append([int(np.argmax(prediction)), int(true)])
-        
+        mlflow.log_metric(key=f'predicted_label_{i}', value=int(np.argmax(prediction)))
+        i+=1
+    
     print(output)
     return output
